4d059ac494f1f0f683511ac7e26d239145bed8ee
merilla-hash <143341055+SMG-hash@users.noreply.github.com>
2024-10-11 18:38:34 +0200

    Update juegoCincoDados.php

--- tareas/juegoCincoDados.php
+++ tareas/juegoCincoDados.php
@@ -6,15 +6,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JuegoCincoDados</title>
     <style>
-
+        <
     </style>
     <?php
+    function dice()
+    {
+        $dice = [
+            "&#9856",
+            "&#9857",
+            "&#9858",
+            "&#9859",
+            "&#9860",
+            "&#9861",
+        ];
+        return $dice;
+    }
+    function playerPick($dice)
+    {
 
+        for ($i = 0; $i < 5; $i++) {
+            $arr[] = $dice[random_int(0, 5)];
+
+        }
+        return $arr;
+    }
+
+    function drawArray($array)
+    {
+        foreach ($array as $valor)
+            echo $valor;
+    }
+
+    function calcWin($arr)
+    {
+    }
+    function drawPick($arr)
+    {
+        for ($i = 0; $i < 2; $i++) {
+            if ($i == 0) {
+                echo "
+                <tr>
+                    <td>Jugador 1</td>
+                    <td>" . drawArray($arr) . "</td>
+                </tr>
+                ";
+            } else {
+                echo "
+                <tr>
+                    <td>Jugador 2</td>
+                    <td>" . drawArray($arr) . "</td>
+                </tr>
+                ";
+            }
+        }
+    }
+    function run()
+    {
+        drawArray(playerPick(dice()));
+        echo "<br>";
+        drawArray(playerPick(dice()));
+    }
     ?>
 </head>
 
 <body>
-
+    <table>
+        <?= run() ?>
+    </table>
 </body>
+
 
 </html>
