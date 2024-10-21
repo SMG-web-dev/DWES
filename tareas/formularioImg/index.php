c13f00c530c4f2ba637c19d3c2a46ba6e90edc05
SMG-hash <143341055+SMG-hash@users.noreply.github.com>
2024-10-21 12:30:17 +0200

    04

--- tareas/formularioImg/index.php
+++ tareas/formularioImg/index.php
@@ -1,2 +1,14 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    header("Location: captura.html");
+    exit();
+} else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $alias = $_POST["alias"];
+    $edad = $_POST["edad"];
+    $armas[] = $_POST["armas"];
+    $magia = $_POST["magia"];
+    $imagen = $_POST["imagen"];
 
+    exit();
+}
