502d5648e608734dae5da4ef1ce1d997a1675c65
SMG-web-dev <smanjon2021@gmail.com>
2024-11-06 20:51:48 +0100

    php-06 done

--- php-06/tarjetas/pagocookie.php
+++ php-06/tarjetas/pagocookie.php
@@ -1,37 +1,48 @@
 <?php
-setcookie("newUser", $user);
+// Verificar si el usuario ha seleccionado una nueva tarjeta mediante el parámetro GET
+if (isset($_GET['nuevatarjeta'])) {
+    // Guardar la selección en una cookie que expire en 5 minutos
+    setcookie("tarjetaUser", $_GET['nuevatarjeta'], time() + 300);
+    // Recargar la página para mostrar la tarjeta sin el parámetro en la URL
+    header("Location: pagocookie.php");
+    exit();
+}
+
+// Función para mostrar el estado actual del método de pago
+function mostrarEstadoPago()
+{
+    if (!isset($_COOKIE['tarjetaUser'])) {
+        // No existe un método de pago en la cookie
+        return "<h3>No tiene un método de pago asignado.</h3>";
+    } else {
+        // Mostrar el método de pago actual usando la cookie
+        $tarjeta = htmlspecialchars($_COOKIE['tarjetaUser']);
+        return "<h3>Su método de pago actual es:</h3><br><img src='imagenes/{$tarjeta}.gif' />";
+    }
+}
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="es">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
     <title>Forma de pago</title>
 </head>
 
 <body>
     <center>
-        <?= isNewUser() ?>
-        </br>
-        <img src='imagenes/visa1.gif' /></a>
-        <h2>SELECCIONE UNA NUEVA TARJETA DE CREDITO </h2><br>
-        <a href='pagosesion?nuevatarjeta=cashu'><img src='imagenes/cashu.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=cirrus1'><img src='imagenes/cirrus1.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=dinersclub'><img src='imagenes/dinersclub.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=mastercard1'><img src='imagenes/mastercard1.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=paypal'><img src='imagenes/paypal.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=visa1'><img src='imagenes/visa1.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=visa_electron'><img src='imagenes/visa_electron.gif' /></a>
+        <!-- Muestra el estado actual del pago -->
+        <?= mostrarEstadoPago() ?>
 
+        <h2>SELECCIONE UNA NUEVA TARJETA DE CRÉDITO</h2><br>
+        <a href='pagocookie.php?nuevatarjeta=cashu'><img src='imagenes/cashu.gif' /></a>&ensp;
+        <a href='pagocookie.php?nuevatarjeta=cirrus1'><img src='imagenes/cirrus1.gif' /></a>&ensp;
+        <a href='pagocookie.php?nuevatarjeta=dinersclub'><img src='imagenes/dinersclub.gif' /></a>&ensp;
+        <a href='pagocookie.php?nuevatarjeta=mastercard1'><img src='imagenes/mastercard1.gif' /></a>&ensp;
+        <a href='pagocookie.php?nuevatarjeta=paypal'><img src='imagenes/paypal.gif' /></a>&ensp;
+        <a href='pagocookie.php?nuevatarjeta=visa1'><img src='imagenes/visa1.gif' /></a>&ensp;
+        <a href='pagocookie.php?nuevatarjeta=visa_electron'><img src='imagenes/visa_electron.gif' /></a>
     </center>
 </body>
 
 </html>
-
-<?php
-function isNewUser(): string
-{
-    (true == false) ?
-        $msg = "<H2>NO TIENE FORMA DE PAGO ASIGNADA</H2>" :
-        $msg = "<H2> SU FORMA DE PAGO ACTUAL ES</H2>";
-    return $msg;
-}
--- php-06/tarjetas/pagosesion.php
+++ php-06/tarjetas/pagosesion.php
@@ -1,35 +1,64 @@
-<html>
+<?php
+session_start();
+
+// Configuración del tiempo de expiración de la sesión en segundos (por ejemplo, 5 minutos)
+$tiempoInactividad = 60; // 1 minutos = 60 segundos
+
+// Verifica si el usuario ha seleccionado una tarjeta y si hay una sesión activa
+if (isset($_GET['nuevatarjeta'])) {
+    $_SESSION['tarjetaUser'] = $_GET['nuevatarjeta'];
+    $_SESSION['ultimo_acceso'] = time(); // Guarda el momento del último acceso
+    header("Location: pagosesion.php"); // Recarga la página para evitar repetir el parámetro
+    exit();
+}
+
+// Si la sesión existe y ha pasado el tiempo de inactividad, se destruye
+if (isset($_SESSION['ultimo_acceso'])) {
+    $tiempoTranscurrido = time() - $_SESSION['ultimo_acceso'];
+    if ($tiempoTranscurrido > $tiempoInactividad) {
+        session_unset();     // Limpia la sesión
+        session_destroy();   // Destruye la sesión
+        header("Location: pagosesion.php"); // Recarga para reflejar el cambio
+        exit();
+    } else {
+        $_SESSION['ultimo_acceso'] = time(); // Actualiza el tiempo de acceso
+    }
+}
+
+// Función para mostrar el estado actual del método de pago
+function mostrarEstadoPago()
+{
+    if (!isset($_SESSION['tarjetaUser'])) {
+        return "<h3>No tiene un método de pago asignado.</h3>";
+    } else {
+        $tarjeta = htmlspecialchars($_SESSION['tarjetaUser']);
+        return "<h3>Su método de pago actual es:</h3><br><img src='imagenes/{$tarjeta}.gif' />";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
     <title>Forma de pago</title>
 </head>
 
 <body>
     <center>
-        <?= isNewUser() ?>
-        </br>
-        <img src='imagenes/visa1.gif' /></a>
-        <h2>SELECCIONE UNA NUEVA TARJETA DE CREDITO </h2><br>
-        <a href='pagosesion?nuevatarjeta=cashu'><img src='imagenes/cashu.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=cirrus1'><img src='imagenes/cirrus1.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=dinersclub'><img src='imagenes/dinersclub.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=mastercard1'><img src='imagenes/mastercard1.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=paypal'><img src='imagenes/paypal.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=visa1'><img src='imagenes/visa1.gif' /></a>&ensp;
-        <a href='pagosesion?nuevatarjeta=visa_electron'><img src='imagenes/visa_electron.gif' /></a>
+        <!-- Muestra el estado actual del pago -->
+        <?= mostrarEstadoPago() ?>
 
+        <h2>SELECCIONE UNA NUEVA TARJETA DE CRÉDITO</h2><br>
+        <a href='pagosesion.php?nuevatarjeta=cashu'><img src='imagenes/cashu.gif' /></a>&ensp;
+        <a href='pagosesion.php?nuevatarjeta=cirrus1'><img src='imagenes/cirrus1.gif' /></a>&ensp;
+        <a href='pagosesion.php?nuevatarjeta=dinersclub'><img src='imagenes/dinersclub.gif' /></a>&ensp;
+        <a href='pagosesion.php?nuevatarjeta=mastercard1'><img src='imagenes/mastercard1.gif' /></a>&ensp;
+        <a href='pagosesion.php?nuevatarjeta=paypal'><img src='imagenes/paypal.gif' /></a>&ensp;
+        <a href='pagosesion.php?nuevatarjeta=visa1'><img src='imagenes/visa1.gif' /></a>&ensp;
+        <a href='pagosesion.php?nuevatarjeta=visa_electron'><img src='imagenes/visa_electron.gif' /></a>
     </center>
 </body>
 
 </html>
-<?php
-
-
-function isNewUser(): string
-{
-    (true == true) ?
-        $msg = "<H2>NO TIENE FORMA DE PAGO ASIGNADA</H2>" :
-        $msg = "<H2> SU FORMA DE PAGO ACTUAL ES</H2>";
-    return $msg;
-}
